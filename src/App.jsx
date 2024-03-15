64dc29bd981abf8b0d4008c73303f62e88d22164
srivatsan <shritha22@yahoo.com>
2024-03-15 11:35:53 +0530

    base set up

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-3xl font-bold   position-absolute top-0 left-0 translate-x-1/2 translate-y-1/2">
+      Welcome to Expense Trak
+    </h1>
+  )
+}
+
+export default App
